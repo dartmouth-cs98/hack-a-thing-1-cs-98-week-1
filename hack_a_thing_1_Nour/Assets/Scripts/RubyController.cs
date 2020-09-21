98319a322f701e965f97e23ab155ced4b7d3383e
Nour Benmohamed <nourbenmohamed@Nours-MacBook-Pro.local>
2020-09-20 23:41:52 -0400

    added a palette, added physics system, added collidors

--- hack_a_thing_1_Nour/Assets/Scripts/RubyController.cs
+++ hack_a_thing_1_Nour/Assets/Scripts/RubyController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal; 
+    float vertical;
+    
     // Start is called before the first frame update
     void Start()
     {
-        // QualitySettings.vSyncCount = 0;
-        // Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
         
     }
 
@@ -16,14 +19,16 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         // to write to console do: Debug.Log();
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector2 position = transform.position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+    }
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        transform.position = position;
-        
+        rigidbody2d.MovePosition(position);
     }
 }
